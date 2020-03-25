c4019615eb152882ee5ad4707a90781f14dd9f91
sloboegen98 <sloboegen98@ya.ru>
2020-03-25 22:45:07 +0300

    add tests

---
+++ Haskell_CPP/test/in/in31.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE TypeFamilies #-}
+
+class Store store where
+  type StoreMonad store :: * -> *
+  new :: a -> (StoreMonad store) (store a)
+  get :: store a -> (StoreMonad store) a
+  put :: store a -> a -> (StoreMonad store) ()
